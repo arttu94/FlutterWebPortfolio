1713f3b54530e9232c5db2d3c9a7d27785085b73
Arturo Glz <arttu.gz94@hotmail.com>
2021-06-16 20:55:03 -0500

    particles adapt to screen area instead of width

--- lib/MK2/components/animated_background.dart
+++ lib/MK2/components/animated_background.dart
@@ -13,18 +13,22 @@ class _AnimatedBackgroundState extends State<AnimatedBackground> {
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
+    double screenArea = screenHeight * screenWidth;
+    double particleCount = (screenArea * 100) / 1500000;
+    double particleSize = (screenArea * 4) / 1500000;
+    double particleConnectDistance = (screenArea * 200) / 1500000;
     return Center(
       child: CircularParticle(
         key: UniqueKey(),
         awayRadius: 0,
-        numberOfParticles: screenWidth <= 600 ? 15 : 55,
+        numberOfParticles: particleCount.clamp(20, 80),
         speedOfParticles: 0.25,
         height: screenHeight,
         width: screenWidth,
         onTapAnimation: false,
         particleColor: Colors.blueGrey.withAlpha(40),
         awayAnimationDuration: Duration(milliseconds: 600),
-        maxParticleSize: screenWidth <= 600 ? 1 : 3,
+        maxParticleSize: particleSize.clamp(2, 3),
         isRandSize: false,
         isRandomColor: false,
         randColorList: [
@@ -38,7 +42,7 @@ class _AnimatedBackgroundState extends State<AnimatedBackground> {
         enableHover: false,
         hoverColor: Colors.white,
         hoverRadius: 10,
-        connectDotsDistance: screenWidth <= 600 ? 100 : 150,
+        connectDotsDistance: particleConnectDistance.clamp(100, 150),
         connectDots: true,
         directionChangeTimeMili: 2500,
       ),
